32c763a3ae30b778c94941985d36800538c98612
brevre <brendamakena09@gmail.com>
2023-09-28 00:08:32 +0200

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+// Define companyName as a constant
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo using let
+let companyCeo = 'Susan Smith';
+
+// Example usage
+console.log(`Company Name: ${companyName}`);
+console.log(`Most Profitable Neighborhood: ${mostProfitableNeighborhood}`);
+console.log(`Company CEO: ${companyCeo}`);
+
+
